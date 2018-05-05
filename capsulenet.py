52ead17514f409103dad326378cf2696706f06d0
Richard Decal <richard.decal@ncf.edu>
2018-05-05 12:13:00 -0400

    implement load notMNIST

--- capsulenet.py
+++ capsulenet.py
@@ -199,6 +199,20 @@ def load_mnist():
     y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
 
+def load_notMNIST():
+    pickle_file = 'notMNIST.pickle'
+
+    with open(pickle_file, 'rb') as f:
+        save = pickle.load(f)
+        train_dataset = save['train_dataset']
+        train_labels = save['train_labels']
+        #valid_dataset = save['valid_dataset']
+        #valid_labels = save['valid_labels']
+        test_dataset = save['test_dataset']
+        test_labels = save['test_labels']
+
+        return (train_dataset, train_labels), (test_dataset, test_labels)
+
 
 if __name__ == "__main__":
     import os
