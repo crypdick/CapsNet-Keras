54888635e3649685b7a4bff94163fa9b189788ac
Richard Decal <richard.decal@ncf.edu>
2018-05-07 21:22:15 -0400

    generalizing with code I wrote for notMNIST

--- capsulenet_vanilla_mnist.py
+++ capsulenet_vanilla_mnist.py
@@ -30,7 +30,7 @@ from sklearn.model_selection import train_test_split
 
 K.set_image_data_format('channels_last')
 
-
+CAPSULE_LAYER_DIMENSIONALITY = 16
 
 def CapsNet(input_shape, n_class, routings):
     """
@@ -43,14 +43,21 @@ def CapsNet(input_shape, n_class, routings):
     """
     x = layers.Input(shape=input_shape)
 
+
+
+    primarycaps_strides = 2
+    primarycaps_channels = CAPSULE_LAYER_DIMENSIONALITY * primarycaps_strides
+    dim_capsuls = 8
+    n_filters = dim_capsuls*primarycaps_channels
+
     # Layer 1: Just a conventional Conv2D layer
-    conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+    conv1 = layers.Conv2D(filters=n_filters, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
 
     # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
-    primarycaps = PrimaryCap(conv1, dim_capsule=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+    primarycaps = PrimaryCap(conv1, dim_capsule=dim_capsuls, n_channels=primarycaps_channels, kernel_size=9, strides=primarycaps_strides, padding='valid')
 
     # Layer 3: Capsule layer. Routing algorithm works here.
-    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=16, routings=routings,
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_capsule=CAPSULE_LAYER_DIMENSIONALITY, routings=routings,
                              name='digitcaps')(primarycaps)
 
     # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
@@ -60,11 +67,12 @@ def CapsNet(input_shape, n_class, routings):
     # Decoder network.
     y = layers.Input(shape=(n_class,))
     masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
+    masked_by_y = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer. For training
     masked = Mask()(digitcaps)  # Mask using the capsule with maximal length. For prediction
 
     # Shared Decoder model in training and prediction
     decoder = models.Sequential(name='decoder')
-    decoder.add(layers.Dense(512, activation='relu', input_dim=16*n_class))
+    decoder.add(layers.Dense(512, activation='relu', input_dim=CAPSULE_LAYER_DIMENSIONALITY * n_class))
     decoder.add(layers.Dense(1024, activation='relu'))
     decoder.add(layers.Dense(np.prod(input_shape), activation='sigmoid'))
     decoder.add(layers.Reshape(target_shape=input_shape, name='out_recon'))
@@ -74,7 +82,7 @@ def CapsNet(input_shape, n_class, routings):
     eval_model = models.Model(x, [out_caps, decoder(masked)])
 
     # manipulate model
-    noise = layers.Input(shape=(n_class, 16))
+    noise = layers.Input(shape=(n_class, CAPSULE_LAYER_DIMENSIONALITY))
     noised_digitcaps = layers.Add()([digitcaps, noise])
     masked_noised_y = Mask()([noised_digitcaps, y])
     manipulate_model = models.Model([x, y, noise], decoder(masked_noised_y))
@@ -176,7 +184,7 @@ def manipulate_latent(model, data, args):
     x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
     noise = np.zeros([1, 10, 16])
     x_recons = []
-    for dim in range(16):
+    for dim in range(CAPSULE_LAYER_DIMENSIONALITY):
         for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
             tmp = np.copy(noise)
             tmp[:,:,dim] = r
@@ -185,7 +193,7 @@ def manipulate_latent(model, data, args):
 
     x_recons = np.concatenate(x_recons)
 
-    img = combine_images(x_recons, height=16)
+    img = combine_images(x_recons, height=CAPSULE_LAYER_DIMENSIONALITY)
     image = img*255
     Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
     print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
@@ -208,30 +216,17 @@ def load_mnist():
 def load_notMNIST_from_npy():
     image_data = np.load("image_data.npy")
     label_data = np.load("label_data.npy")
-    image_data = np.expand_dims(image_data, axis=4)
-    label_data = np.expand_dims(label_data, axis=1)
+    label_data = to_categorical(label_data.astype('float32'))  # 1 hot encode
+    image_data = image_data.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    #label_data = np.expand_dims(label_data, axis=1)
 
     data_train, data_test, labels_train, labels_test = train_test_split(image_data,
                                                                         label_data,
                                                                         test_size=0.20,
                                                                         random_state=42)
 
-    return data_train, data_test, labels_train, labels_test
+    return data_train, labels_train, data_test, labels_test
 
-
-def load_notMNIST():
-    pickle_file = 'notMNIST.pickle'
-
-    with open(pickle_file, 'rb') as f:
-        save = pickle.load(f)
-        train_dataset = save['train_dataset']
-        train_labels = save['train_labels']
-        #valid_dataset = save['valid_dataset']
-        #valid_labels = save['valid_labels']
-        test_dataset = save['test_dataset']
-        test_labels = save['test_labels']
-
-        return (train_dataset, train_labels), (test_dataset, test_labels)
 
 
 if __name__ == "__main__":
@@ -271,7 +266,7 @@ if __name__ == "__main__":
 
     # load data
     (x_train, y_train), (x_test, y_test) = load_mnist()
-    x_train1, y_train1, x_test1, y_test1 = load_notMNIST_from_npy()
+    #x_train, y_train, x_test, y_test = load_notMNIST_from_npy()
 
     # define model
     model, eval_model, manipulate_model = CapsNet(input_shape=x_train.shape[1:],
@@ -289,4 +284,3 @@ if __name__ == "__main__":
             print('No weights are provided. Will test using random initialized weights.')
         manipulate_latent(manipulate_model, (x_test, y_test), args)
         test(model=eval_model, data=(x_test, y_test), args=args)
-
